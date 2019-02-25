72de14d2d35a2dea8cdf64aea7df677d88ee06b6
Nara <naraa_520@yahoo.com>
2019-02-24 21:58:45 -0800

    Removed the require pry

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,6 +1,5 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
-require 'pry'
 def array_equals(array1, array2)
   if array1.nil? && array2.nil?
     return true
